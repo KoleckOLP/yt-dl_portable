3eb94e33f29415d77015b471f65a8e3adbf9f44b
koleq(WORK-D) <o.kurz@atlas.cz>
2022-06-29 08:45:28 +0200

    Improve downloading yt-dl closes #1

--- yt-dl_portable.bat
+++ yt-dl_portable.bat
@@ -1,3 +1,9 @@
 @echo off
 
+REM this checks if the yt-dl folder is empty and if yes downloads yt-dl
+dir /b /s /a "yt-dl" | findstr .>nul || (
+  git clone -b testing https://github.com/koleckolp/yt-dl
+)
+
+REM starts yt-dl without popping up cmd
 start /b python\pythonw.exe yt-dl\gui.py
